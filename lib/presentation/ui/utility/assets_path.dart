6f69117c1e0082a5af814e50a8044bb21a53adce
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-11-11 23:35:37 +0600

    home screen and bottom nab bar screen ui has been done

--- lib/presentation/ui/utility/assets_path.dart
+++ lib/presentation/ui/utility/assets_path.dart
@@ -6,4 +6,5 @@ class AssetsPath {
   static String facebookLogoSVG = 'assets/images/facebook_logo.svg';
   static String googleLogoSVG = 'assets/images/google_logo.svg';
   static String appleLogoSVG = 'assets/images/apple_logo.svg';
+  static String profileLogo = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYSfjGho8IEoYfw9R6L3mxeEkHUucNaClE5MSF9y8SDN0y0o3Zii6vEur84GJ9VqiitJj2xwMmLJFBXJ8n4cfx2fX9fyV_a6TqSVAOZCvjKRWcEbndma6phy6zXlVhDwAXJA8YerABUDtCU6uqVNLtJkXga7u7wuFff-LxbW6hxXAs7mcjPTQ03wcUMw/s16000/deepakiya.jpg";
 }
