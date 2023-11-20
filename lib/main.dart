87907c06d5a99e983643540b30c95762e338eb69
Mujahedul Islam <99077742+muj-i@users.noreply.github.com>
2023-11-20 22:16:17 +0600

    firebase integration initial done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_talk/application/app.dart';
+import 'package:flutter_talk/firebase_options.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FlutterTalk());
 }
